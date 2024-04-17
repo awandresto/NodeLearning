d9bb492164284fb5c9b8199481413955e879392a
awandresto <awandresto@gmail.com>
2024-04-17 23:14:35 +0300

    Homework_07 - Login and register routes

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,10 @@ import hpp from 'hpp';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { AppDataSource } from './data-source';
+import routesAuth from './routes/auth';
 
 const app: Express = express();
+const port: number = parseInt(process.env.PORT || '3000', 10);
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -20,15 +22,15 @@ app.use(compression());
 app.use(helmet());
 app.use(hpp());
 
+app.use('/auth', routesAuth);
+
 app.get('/', (req, res) => {
     res.send('Hello from the server!');
 });
 
-const port: number = parseInt(process.env.PORT || '3000', 10);
-
 AppDataSource.initialize()
     .then(async (data) => {
-        app.listen(port, _ => {
+        app.listen(port, () => {
             console.log(`Server is running on http://localhost:${port}`);
         });
     })
---
+++ src/controllers/auth-controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express';
+import * as authService from '../services/auth-service';
+import { storeRefreshedToken } from '../utils/token/token-management';
+import { ErrorMessage } from '../utils/constants/constants';
+
+export const register = async (req: Request, res: Response) => {
+    try {
+        const registerUserDto: any = req.body as any;
+        const newUser = await authService.registerUser(registerUserDto);
+
+        res.status(201).json({ message: 'User registered successfully', user: newUser });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(ErrorMessage.errorRegisteringUser);
+    }
+};
+
+export const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const user = await authService.loginUser(email, password);
+
+    if (user) {
+        const accessToken = authService.generateAccessToken({ id: user.id, email: user.email });
+        const refreshToken = authService.generateRefreshToken({ id: user.id, email: user.email });
+
+        await storeRefreshedToken(user.id, refreshToken);
+        res.json({ accessToken, refreshToken });
+    } else {
+        res.status(400).send(ErrorMessage.errorInvalidCredentials);
+    }
+}
